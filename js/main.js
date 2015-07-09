2a1752e3235add97d63162d9e3fd320c7d3462ca
Jamie Brauckmuller <jamiebrauckmuller@gmail.com>
2015-07-08 21:12:11 -0400

    Update CSS, Index and JS

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-var header = document.getElementById('head');
-var i = 0;
+var fName = document.getElementsByName("fname")[0];
+var lName = document.getElementsByName("lname")[0];
+var eMail = document.getElementsByName("email")[0];
+var textMessage = document.getElementsByName("message")[0];
 
-header.addEventListener("click", function() {
-	i++;
-	var node = document.createElement("p");
-    var textnode = document.createTextNode("This is click number " + i);
-    node.appendChild(textnode);
-    document.getElementById("paragraph").appendChild(node);
+document.getElementById("info").addEventListener("submit", function(event) {
+	event.preventDefault();
+	console.log("The first name is:" + fName.value);
+	console.log("The last name is:" + lName.value);
+	console.log("The email is:" + eMail.value);
+	console.log("The message is:" + textMessage.value);
 });
